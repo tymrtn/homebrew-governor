5da53d5424caf87f5173fb15e6d106c5ff8ec5f3
tymrtn <ty@tmrtn.com>
2026-03-26 10:13:51 +0100

    fix: real binary SHA and release URL

--- Formula/governor.rb
+++ Formula/governor.rb
@@ -7,7 +7,7 @@ class Governor < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/tymrtn/governor/releases/download/v0.1.0/governor-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "57b49c0cf275d82809b97f875c1a379629af45af4fa989215545a1c668220122"
     end
   end
 
